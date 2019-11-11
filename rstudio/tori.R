17bf455817be1c0a770ab25a1421f888326fcc4a
marltake <otake.shigenori@gmail.com>
2019-11-11 19:56:19 +0900

    align 2space indent

--- rstudio/tori.R
+++ rstudio/tori.R
@@ -31,186 +31,186 @@ library(tuneR)
 setwd("/home/rstudio/data")
 
 start <- function(path_wav){
-## directory and files ##############
-dir_cur <- getwd()
-dir_dist <- dirname(path_wav)
-basename_wav <- basename(path_wav)
-dirname_wav  <- dirname(path_wav)
-filebody_wav <- tools::file_path_sans_ext(basename_wav)
-setwd(dir_dist)
+  ## directory and files ##############
+  dir_cur <- getwd()
+  dir_dist <- dirname(path_wav)
+  basename_wav <- basename(path_wav)
+  dirname_wav  <- dirname(path_wav)
+  filebody_wav <- tools::file_path_sans_ext(basename_wav)
+  setwd(dir_dist)
 
-### parameter file laod ####
-file_param      <- stringr::str_replace(path_wav, ".wav", ".psgrm")
-if( ! file.exists(file_param) ){
-  stop(paste("No file:", file_param))
-}else{
-  load(file = file_param)
-};
+  ### parameter file laod ####
+  file_param      <- stringr::str_replace(path_wav, ".wav", ".psgrm")
+  if( ! file.exists(file_param) ){
+    stop(paste("No file:", file_param))
+  }else{
+    load(file = file_param)
+  };
 
-### output file csv) ####
-csvfile_output  <- stringr::str_replace(path_wav, ".wav", ".csv")
-if(file.exists(csvfile_output)) {cat("ファイルに追記します:", csvfile_output)};
+  ### output file csv) ####
+  csvfile_output  <- stringr::str_replace(path_wav, ".wav", ".csv")
+  if(file.exists(csvfile_output)) {cat("ファイルに追記します:", csvfile_output)};
 
-### Recordint date and time ####
-file_info <- stringr::str_split(filebody_wav, "[_-]") %>% unlist;
-date_record <- file_info[1]; # 録音日
-time_start  <- paste(date_record, file_info[2], sep =" ")
-time_start  <- strptime(time_start, format="%y%m%d %H%M%S"); 
-time_end    <- paste(date_record, file_info[3], sep = " "); # 録音終了時間
-time_end    <- strptime( time_end, format="%y%m%d %H%M%S"); # 録音開始時刻
+  ### Recordint date and time ####
+  file_info <- stringr::str_split(filebody_wav, "[_-]") %>% unlist;
+  date_record <- file_info[1]; # 録音日
+  time_start  <- paste(date_record, file_info[2], sep =" ")
+  time_start  <- strptime(time_start, format="%y%m%d %H%M%S"); 
+  time_end    <- paste(date_record, file_info[3], sep = " "); # 録音終了時間
+  time_end    <- strptime( time_end, format="%y%m%d %H%M%S"); # 録音開始時刻
 
-### png file setting ######
-pngs <- list.files(pattern = paste0(filebody_wav, "_P[0-9][0-9].png"))
-num_png <- length(pngs)
+  ### png file setting ######
+  pngs <- list.files(pattern = paste0(filebody_wav, "_P[0-9][0-9].png"))
+  num_png <- length(pngs)
 
-### time setting of toriR ######
-now <- Sys.time()
-txt_kikimimi <- c(
-  paste0("# プログラム:programname=", programname, version, "\n", 
-         "# 解析開始時間:StartTime_toriR=", now, "\n",
-         format(time_start,"%Y.%m.%d,%H:%M:%S,R,,\n")
-  )
-)
-cat(txt_kikimimi, file = csvfile_output, append = TRUE)
-
-### windowの表示画面上の場所指定 ####
-if (windows_a_page == 4){
-  xl <- 0.044; xr <- 0.93;  
-  yb <- 0.805; yt <- 0.978;
-  dy <- 0.25;
-}
-images <- list(1:num_png)
-for (num_page in 1:num_png){
-  images[[num_page]] <- readPNG(pngs[num_page])
-}
-
-### toriRの結果データベース####
-db <- tribble(
-  ~num_png, ~x, ~y, ~label,~col_text,
-  1,        0,   0,  "temp", "white"
-)
-for (num_page in 1:num_png){
-  # for (num_page in 1:2){
-  flag_page_end <- FALSE;
-  pngfile_input  <- pngs[num_page];
-  cat("R> num_page = ",num_page,"\n")
-  cat("R> ", pngfile_input, "\n")
-  cat(paste0("# スペクトログラム:spectrogram=", pngfile_input, "\n"), file = csvfile_output, append =TRUE)
-  num_page <-   stringr::str_sub(pngfile_input, start = 23, end = 24) %>% as.numeric()
-  par(mar=c(0, 0, 0, 0)); #外側のマージンをゼロにする
-  plot(NULL
-       , xlim = c(0,1), ylim = c(0, 1)
-       , xlab = "", ylab = "", yaxt = "n", xaxt = "n",xaxs = 'i', yaxs = 'i'
-  )
-  rasterImage(images[[num_page]], 0, 0, 1, 1)
-  for( i in 0:(windows_a_page-1)){
-    rect( xl, yb - i * dy, xr, yt - i * dy, col="transparent", border = "red")
-  }
-  while (flag_page_end == FALSE){
-    cat("R> 声紋をクリック。無ければ範囲外（白い部分）をクリック。またはESPキー");
-    z <- locator(n=1);
-    if (is.null(z) == TRUE){# ESCが押されたなら
-      flag_page_end <- TRUE
-      break;
-    }
-    x <- z$x; 
-    y <- z$y; 
-    y_position <- "out_of_range"
-    for( k in 0:(windows_a_page - 1)) {# locator()の場所判定
-      if(yb - k * dy <= y  && y < yt - k * dy) {y_position <- k}
-    }
-    if(y_position == "out_of_range"){break};# 範囲外選択
-    points(x, y, col="RED", pch = 20);
-    col_text <- "RED"; #抽出種名の文字の色
-    
-    time_offset <- (num_page - 1) * window_time * windows_a_page + window_time * y_position
-    second_locator <- (time_offset + (x - xl) * (window_time)/(xr - xl)) 
-    if(second_locator < 0)(length_preplay <- 0)
-    str_play_trim <- "MUST BE REMOVED SOX/PLAY"
-    txt_play <- str_play_trim;  # (os, volume, path_wav, second_locator - length_preplay, length_play)
-    time_locator   <- (time_offset + (x - xl) * (window_time)/(xr - xl) + time_start) %>% format("%Y.%m.%d,%H:%M:%S")
-    freq_locator   <- (f_lcf + (y - (yb - y_position * dy)) * (f_hcf - f_lcf)/(yt - yb))
-    
-    answer <- menu(spices, title="\nR> 種類を選択してください:")
-    # cat(paste0("Select:", spices[answer]))
-    while(spices[answer] == "再生") {#再生
-      cat (txt_play);
-      cat (paste0("# 再生:", txt_play, "\n"), file = csvfile_output, append = TRUE);
-      answer <- menu(spices, title="R> 種類を再選択してください");
-      cat(paste0("Reselection = ", spices[answer],"\n"))
-    }
-    if(spices[answer] ==  "WAY｜保存") {#
-      time_locator_file  <- (time_offset + (x - xl) * (window_time)/(xr - xl) + time_start) %>% format("%y%m%d_%H%M%S")
-      file_sox_save <- paste0(time_locator_file, "｜WAY.wav")
-      txt_sox_save  <- sprintf("sox -v %d %s %s trim %s %d", volume, path_wav, file_sox_save, second_locator - length_preplay, length_play)
-      system(txt_sox_save);
-      col_text <- "YELLOW"
-    }
-    if (answer == "0") {
-      flag_page_end <- TRUE;
-      break;
-    }else{
-      txt_kikimimi <- sprintf("%s,%s,F=%d[Hz]\n", time_locator , spices[answer], as.integer(freq_locator))
-      cat(paste0("\nkikimimi>",txt_kikimimi))
-      cat(txt_kikimimi, file = csvfile_output, append = TRUE)
-      db <- rbind(db,c(num_page, x, y, spices[answer], col_text))
-      text(x, y + 0.03, label = spices[answer], col= col_text); # 選択した文字列を図中に表示
-    }
-  }
-  if(num_page == num_png){
-    dev.off()
-    before <- now
-    now <- Sys.time()
-    txt_ending <- paste0(
-      format(time_end,"%Y.%m.%d,%H:%M:%S,STOP,,\n"),
-      "# 終了解析時間:StopTime_toriR=", now, "\n",
-      "# 分析時間:AnalysisTime_toriR=", format(as.numeric(difftime(now, before, unit="min")), digit=2), "min.\n"
+  ### time setting of toriR ######
+  now <- Sys.time()
+  txt_kikimimi <- c(
+    paste0("# プログラム:programname=", programname, version, "\n", 
+          "# 解析開始時間:StartTime_toriR=", now, "\n",
+          format(time_start,"%Y.%m.%d,%H:%M:%S,R,,\n")
     )
-    cat(txt_ending, file = csvfile_output, append = TRUE)
-    cat(txt_ending)
+  )
+  cat(txt_kikimimi, file = csvfile_output, append = TRUE)
+
+  ### windowの表示画面上の場所指定 ####
+  if (windows_a_page == 4){
+    xl <- 0.044; xr <- 0.93;  
+    yb <- 0.805; yt <- 0.978;
+    dy <- 0.25;
   }
-}
+  images <- list(1:num_png)
+  for (num_page in 1:num_png){
+    images[[num_page]] <- readPNG(pngs[num_page])
+  }
 
-
-##画像に抽出結果を出力する ####
-answer <- menu(c("上書き", "終了"), title="\nR> 画像ファイルの処理")
-if (answer == 1){
-  db <- db[-1,];
-  cat("画像ファイルに結果を出力します")
+  ### toriRの結果データベース####
+  db <- tribble(
+    ~num_png, ~x, ~y, ~label,~col_text,
+    1,        0,   0,  "temp", "white"
+  )
   for (num_page in 1:num_png){
     # for (num_page in 1:2){
+    flag_page_end <- FALSE;
     pngfile_input  <- pngs[num_page];
-    pngfile_output <- stringr::str_replace(pngfile_input, ".png", "_toriR.png");
-    pngfile_output <- pngfile_input;
-    png(pngfile_output, width = width_image, height = windows_a_page * height_image, bg="white")
+    cat("R> num_page = ",num_page,"\n")
+    cat("R> ", pngfile_input, "\n")
+    cat(paste0("# スペクトログラム:spectrogram=", pngfile_input, "\n"), file = csvfile_output, append =TRUE)
+    num_page <-   stringr::str_sub(pngfile_input, start = 23, end = 24) %>% as.numeric()
     par(mar=c(0, 0, 0, 0)); #外側のマージンをゼロにする
-    plot(NULL, xlim = c(0,1), ylim = c(0, 1), xlab = "a", ylab = "", yaxt = "n", xaxt = "n",xaxs = 'i', yaxs = 'i')
+    plot(NULL
+        , xlim = c(0,1), ylim = c(0, 1)
+        , xlab = "", ylab = "", yaxt = "n", xaxt = "n",xaxs = 'i', yaxs = 'i'
+    )
     rasterImage(images[[num_page]], 0, 0, 1, 1)
-    text(0.9,0.99,label="toriR",col="RED")
-    for (ii in 1:nrow(db)){
-      w <- db[ii,];
-      if ( num_page == w$num_png ){
-        points(w$x, w$y, col=w$col_text, pch = 20);
-        text(as.numeric(w$x), as.numeric(w$y) + 0.03, label = w$label, col= w$col_text);
+    for( i in 0:(windows_a_page-1)){
+      rect( xl, yb - i * dy, xr, yt - i * dy, col="transparent", border = "red")
+    }
+    while (flag_page_end == FALSE){
+      cat("R> 声紋をクリック。無ければ範囲外（白い部分）をクリック。またはESPキー");
+      z <- locator(n=1);
+      if (is.null(z) == TRUE){# ESCが押されたなら
+        flag_page_end <- TRUE
+        break;
+      }
+      x <- z$x; 
+      y <- z$y; 
+      y_position <- "out_of_range"
+      for( k in 0:(windows_a_page - 1)) {# locator()の場所判定
+        if(yb - k * dy <= y  && y < yt - k * dy) {y_position <- k}
+      }
+      if(y_position == "out_of_range"){break};# 範囲外選択
+      points(x, y, col="RED", pch = 20);
+      col_text <- "RED"; #抽出種名の文字の色
+      
+      time_offset <- (num_page - 1) * window_time * windows_a_page + window_time * y_position
+      second_locator <- (time_offset + (x - xl) * (window_time)/(xr - xl)) 
+      if(second_locator < 0)(length_preplay <- 0)
+      str_play_trim <- "MUST BE REMOVED SOX/PLAY"
+      txt_play <- str_play_trim;  # (os, volume, path_wav, second_locator - length_preplay, length_play)
+      time_locator   <- (time_offset + (x - xl) * (window_time)/(xr - xl) + time_start) %>% format("%Y.%m.%d,%H:%M:%S")
+      freq_locator   <- (f_lcf + (y - (yb - y_position * dy)) * (f_hcf - f_lcf)/(yt - yb))
+      
+      answer <- menu(spices, title="\nR> 種類を選択してください:")
+      # cat(paste0("Select:", spices[answer]))
+      while(spices[answer] == "再生") {#再生
+        cat (txt_play);
+        cat (paste0("# 再生:", txt_play, "\n"), file = csvfile_output, append = TRUE);
+        answer <- menu(spices, title="R> 種類を再選択してください");
+        cat(paste0("Reselection = ", spices[answer],"\n"))
+      }
+      if(spices[answer] ==  "WAY｜保存") {#
+        time_locator_file  <- (time_offset + (x - xl) * (window_time)/(xr - xl) + time_start) %>% format("%y%m%d_%H%M%S")
+        file_sox_save <- paste0(time_locator_file, "｜WAY.wav")
+        txt_sox_save  <- sprintf("sox -v %d %s %s trim %s %d", volume, path_wav, file_sox_save, second_locator - length_preplay, length_play)
+        system(txt_sox_save);
+        col_text <- "YELLOW"
+      }
+      if (answer == "0") {
+        flag_page_end <- TRUE;
+        break;
+      }else{
+        txt_kikimimi <- sprintf("%s,%s,F=%d[Hz]\n", time_locator , spices[answer], as.integer(freq_locator))
+        cat(paste0("\nkikimimi>",txt_kikimimi))
+        cat(txt_kikimimi, file = csvfile_output, append = TRUE)
+        db <- rbind(db,c(num_page, x, y, spices[answer], col_text))
+        text(x, y + 0.03, label = spices[answer], col= col_text); # 選択した文字列を図中に表示
       }
     }
-    dev.off()
+    if(num_page == num_png){
+      dev.off()
+      before <- now
+      now <- Sys.time()
+      txt_ending <- paste0(
+        format(time_end,"%Y.%m.%d,%H:%M:%S,STOP,,\n"),
+        "# 終了解析時間:StopTime_toriR=", now, "\n",
+        "# 分析時間:AnalysisTime_toriR=", format(as.numeric(difftime(now, before, unit="min")), digit=2), "min.\n"
+      )
+      cat(txt_ending, file = csvfile_output, append = TRUE)
+      cat(txt_ending)
+    }
   }
-}
-setwd(dir_cur)
+
+
+  ##画像に抽出結果を出力する ####
+  answer <- menu(c("上書き", "終了"), title="\nR> 画像ファイルの処理")
+  if (answer == 1){
+    db <- db[-1,];
+    cat("画像ファイルに結果を出力します")
+    for (num_page in 1:num_png){
+      # for (num_page in 1:2){
+      pngfile_input  <- pngs[num_page];
+      pngfile_output <- stringr::str_replace(pngfile_input, ".png", "_toriR.png");
+      pngfile_output <- pngfile_input;
+      png(pngfile_output, width = width_image, height = windows_a_page * height_image, bg="white")
+      par(mar=c(0, 0, 0, 0)); #外側のマージンをゼロにする
+      plot(NULL, xlim = c(0,1), ylim = c(0, 1), xlab = "a", ylab = "", yaxt = "n", xaxt = "n",xaxs = 'i', yaxs = 'i')
+      rasterImage(images[[num_page]], 0, 0, 1, 1)
+      text(0.9,0.99,label="toriR",col="RED")
+      for (ii in 1:nrow(db)){
+        w <- db[ii,];
+        if ( num_page == w$num_png ){
+          points(w$x, w$y, col=w$col_text, pch = 20);
+          text(as.numeric(w$x), as.numeric(w$y) + 0.03, label = w$label, col= w$col_text);
+        }
+      }
+      dev.off()
+    }
+  }
+  setwd(dir_cur)
 }
 
 save_and_play <- function(wavedata, basename, start, period) {
-    stop = start + period
-    # wavefile = paste(basename, "_", as.character(start), "-", as.character(stop), ".wav", sep="")
-    wavefile = sprintf("%s_%04d-%04d.wav", basename, start, stop)
-    start = wavedata@samp.rate * start + 1
-    stop = wavedata@samp.rate * stop
-    tuneR::writeWave(wavedata[start:stop], filename = wavefile)
-    wave_url = paste("http://localhost:8000/", wavefile, sep = "")
-    audio = tags$audio(
-    controls="", autoplay="", name="media", 
-    tags$source(src=wave_url, type="audio/x-wav")
-    )
+  stop = start + period
+  # wavefile = paste(basename, "_", as.character(start), "-", as.character(stop), ".wav", sep="")
+  wavefile = sprintf("%s_%04d-%04d.wav", basename, start, stop)
+  start = wavedata@samp.rate * start + 1
+  stop = wavedata@samp.rate * stop
+  tuneR::writeWave(wavedata[start:stop], filename = wavefile)
+  wave_url = paste("http://localhost:8000/", wavefile, sep = "")
+  audio = tags$audio(
+  controls="", autoplay="", name="media", 
+  tags$source(src=wave_url, type="audio/x-wav")
+  )
   htmltools::html_print(audio)
 }
